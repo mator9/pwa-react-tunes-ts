0ad8b8820b464d9b1c128644cb71b0073343ef29
Martin Rybovic <martin.rybovic@exxeta.com>
2023-08-04 16:17:02 +0200

    added dummy notif

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import logo from "./assets/logo.svg";
 //styles & asssets
 import "./App.scss";
@@ -13,13 +13,44 @@ import Tunes from "./views/Tunes";
 import About from "./views/About";
 
 function App() {
+  useEffect(() => {
+    // Requesting permission for Notifications after clicking on the button
+    const button = document.getElementById("notifications") as HTMLElement;
+    button.addEventListener("click", () => {
+      Notification.requestPermission().then((result) => {
+        if (result === "granted") {
+          randomNotification();
+        }
+      });
+    });
+    // Setting up random Notification
+    function randomNotification() {
+      // const randomItem = Math.floor(Math.random() * games.length);
+      // const notifTitle = games[randomItem].name;
+      // const notifBody = `Created by ${games[randomItem].author}.`;
+      // const notifImg = `data/img/${games[randomItem].slug}.jpg`;
+  
+      const notifTitle = "Now my title";
+  
+      const notifBody = `Created by test.`;
+      const options = {
+        body: notifBody,
+      };
+      // icon: notifImg,
+      new Notification(notifTitle, options);
+      setTimeout(randomNotification, 30000);
+    }
+
+  }, []);
+
+
   return (
     <div className="App">
       <header>
-        <TheNavigation/>
-
+        <TheNavigation />
       </header>
       <main className="content">
+        <button id="notifications">Request dummy notifications</button>
         <Routes>
           <Route path="/" Component={Home} />
           <Route path="/tunes" Component={Tunes} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { register as registerServiceWorker } from './serviceWorkerRegistration';
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { register as registerServiceWorker } from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -28,4 +26,3 @@ root.render(
 // }
 
 registerServiceWorker();
-
