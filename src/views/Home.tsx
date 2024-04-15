715e764844a681d1d865127755e64b839ebda253
Martin Rybovic <martin.rybovic@exxeta.com>
2024-04-15 18:22:55 +0200

    tested changing top

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.scss";
 import logo from "../assets/logo.svg";
 
@@ -6,15 +6,27 @@ type Props = {};
 
 const Home = (props: Props) => {
   const [title] = useState("Test this !");
+  const [themeColor, setThemeColor] = useState("#408c31");
 
+  const setTheme = () => {
+    setThemeColor("#31a816"); // Set the new theme color
+  };
   // const [title, setTitle] = useState("Test this !");
 //   useEffect(() => {
 //     document.title = title;
 //   });
   //   const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+    if (metaThemeColor) {
+      metaThemeColor.setAttribute("content", themeColor);
+    }
+  }, [themeColor]); // Re-run this effect whenever themeColor changes
+  
   return (
     <div className="home">
-      <img className="logo" src={logo} alt="react logo"></img>
+      <img onClick={setTheme} className="logo" src={logo} alt="react logo"></img>
       {/* <h1 onClick={() => setTitle("asd")}>{title}</h1> */}
       {/* <h1
         onClick={() => {
